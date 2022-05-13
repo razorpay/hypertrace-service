41cda4ad24b0f356df66eec54782cdf87690563f
Harnoor Singh <harnoor.singh@razorpay.com>
2022-05-13 13:25:10 +0530

    bump

--- hypertrace-data-config-service/build.gradle.kts
+++ hypertrace-data-config-service/build.gradle.kts
@@ -57,7 +57,7 @@ hypertraceDocker {
 fun versionBanner(): String {
   val os = com.bmuschko.gradle.docker.shaded.org.apache.commons.io.output.ByteArrayOutputStream()
   project.exec {
-    commandLine = "git describe --tags".split(" ")
+    commandLine = "git describe --tags --abbrev=0".split(" ")
     standardOutput = os
   }
   return String(os.toByteArray()).trim()
